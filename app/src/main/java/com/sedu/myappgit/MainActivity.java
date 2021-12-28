9d7c001d9d582f7af6ccbee4300f132d9edd50de
joey <tlh0jit@naver.com>
2021-12-29 02:02:53 +0900

    Initial commit

---
+++ app/src/main/java/com/sedu/myappgit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sedu.myappgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int doi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
